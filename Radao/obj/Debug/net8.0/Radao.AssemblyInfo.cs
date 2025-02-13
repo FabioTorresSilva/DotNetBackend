707de27641df5da95a7d363fe7a54d392a908bba
Fábio Silva <121696024+FabioTorresSilva@users.noreply.github.com>
2025-02-13 11:24:54 +0000

    docker

--- Radao/obj/Debug/net8.0/Radao.AssemblyInfo.cs
+++ Radao/obj/Debug/net8.0/Radao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Radao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ef065fb837c56dd413976b32d2b534734994a51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+178977c3b9fd862184a08958f9272403c6be2c5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Radao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Radao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

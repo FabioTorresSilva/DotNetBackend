fb7b39494cc956fae35a0e7c33f345bb1d1df12f
Fábio Lindeza <129009704+fabionlindeza@users.noreply.github.com>
2025-02-13 14:51:46 +0000

    Added Fountain model with properties
    - Created the Fountain class with properties for Id, Description, SusceptibilityIndex, Address, WaterAnalysis, Device, IsDrinkable, Latitude, and Longitude.
    - Applied [Required] attribute to enforce necessary fields and added length constraints where applicable.
    - Established relationships with Address, WaterAnalysis, and Device models via foreign keys.

--- Radao/Models/Fountain.cs
+++ Radao/Models/Fountain.cs
@@ -1,6 +1,42 @@
-﻿namespace Radao.Models
+﻿using Radao.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Radao.Models
 {
     public class Fountain
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        public SusceptibilityIndex SusceptibilityIndex { get; set; }
+
+        [Required]
+        public Address Address { get; set; }
+
+        [Required]
+        public int AddressId { get; set; }
+
+        [Required]
+        public List<WaterAnalysis> WaterAnalysis { get; set; }
+
+        [Required]
+        public Device Device { get; set; }
+
+        [Required]
+        public int DeviceId { get; set; }
+
+        [Required]
+        public bool IsDrinkable { get; set; }
+
+        [Required]
+        public double Latitude { get; set; }
+
+        [Required]
+        public double Longitude { get; set; }
     }
 }

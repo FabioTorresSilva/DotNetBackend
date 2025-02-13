8702defcd5ad8cebe36e82caf795c30067374bc5
Fábio Lindeza <129009704+fabionlindeza@users.noreply.github.com>
2025-02-13 13:10:20 +0000

    Added Address model with properties
    - Added properties for Id, District, Country, and CivilParish, with validation attributes to ensure non-null and maximum length constraints.
    - Introduced a List<Fountain> property to store related fountains for each address.
    - Applied the [Required] and [MaxLength(50)] annotations for necessary fields to ensure data integrity.

--- Radao/Models/Address.cs
+++ Radao/Models/Address.cs
@@ -1,7 +1,25 @@
-﻿namespace Radao.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Radao.Models
 {
     public class Address
     {
-        doamdaodma
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string District { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Country { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string CivilParish { get; set; }
+
+        [Required]
+        public List<Fountain> Fountains { get; set; }
     }
 }

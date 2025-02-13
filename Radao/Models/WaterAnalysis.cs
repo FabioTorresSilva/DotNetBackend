add344b0e91f4b5e729fed11f2bb51966309928d
Fábio Lindeza <129009704+fabionlindeza@users.noreply.github.com>
2025-02-13 14:56:11 +0000

    Added WaterAnalysis model with properties
    - Created the WaterAnalysis class with properties for Id, RadonConcentration, Fountain, Date, and Device.
    - Applied [Required] attribute to ensure all properties are not null.
    - Added a foreign key DeviceId to establish a relationship with the Device model.

--- Radao/Models/WaterAnalysis.cs
+++ Radao/Models/WaterAnalysis.cs
@@ -1,6 +1,25 @@
-﻿namespace Radao.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Radao.Models
 {
     public class WaterAnalysis
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public double RadonConcentration { get; set; }
+
+        [Required]
+        public Fountain Fountain { get; set; }
+
+        [Required]
+        public DateOnly Date { get; set; }
+
+        [Required]
+        public Device Device { get; set; }
+
+        [Required]
+        public int DeviceId { get; set; }
     }
 }

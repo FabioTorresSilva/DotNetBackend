5fe727a4d096de03fefc6eaf3e6506c2a480ee02
Fábio Lindeza <129009704+fabionlindeza@users.noreply.github.com>
2025-02-13 14:54:35 +0000

    Added Device model with properties
    - Created the Device class with properties for Id, Description, and ExpirationDate.
    - Applied [Required] attribute to ensure Description and ExpirationDate are not null.
    - Added a [MaxLength(500)] constraint to the - Description property to limit the maximum length to 500 characters.

--- Radao/Models/Device.cs
+++ Radao/Models/Device.cs
@@ -1,6 +1,17 @@
-﻿namespace Radao.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Radao.Models
 {
     public class Device
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        public DateOnly ExpirationDate { get; set; }
     }
 }

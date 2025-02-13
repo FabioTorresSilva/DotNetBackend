8a2e7c5932db3db21db4eb5a3306959eda43d20c
João Lomba <lomba95joao@gmail.com>
2025-02-13 13:56:40 +0000

    Enum added

--- Radao/Enums/SusceptibilityIndex.cs
+++ Radao/Enums/SusceptibilityIndex.cs
@@ -1,6 +1,12 @@
 ﻿namespace Radao.Enums
 {
+    /// <summary>
+    /// Enum that represent the different susceptibility indexes 
+    /// </summary>
     public enum SusceptibilityIndex
     {
+        Low,
+        Moderate,
+        High
     }
 }

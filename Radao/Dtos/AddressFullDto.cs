d3d214887ea8ce1301f6e75fcb110b6619bec0af
João Lomba <lomba95joao@gmail.com>
2025-02-13 14:17:26 +0000

    All arguments Dtos files added

---
+++ Radao/Dtos/AddressFullDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Radao.Dtos
+{
+    /// <summary>
+    /// Class that represents an Address data transfer object with all of the arguments of the model
+    /// </summary>
+    public class AddressFullDto
+    {
+    }
+}
---
+++ Radao/Dtos/FountainFullDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Radao.Dtos
+{
+    /// <summary>
+    /// Class that represents an Fountain data transfer object with all of the arguments of the model
+    /// </summary>
+    public class FountainFullDto
+    {
+    }
+}

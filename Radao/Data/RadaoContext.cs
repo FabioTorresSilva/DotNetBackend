4d45998f72b85206ba8fdbb64d02fb45afa6c25d
João Lomba <lomba95joao@gmail.com>
2025-02-13 12:16:56 +0000

    Radao Context updated

--- Radao/Data/RadaoContext.cs
+++ Radao/Data/RadaoContext.cs
@@ -1,12 +1,49 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Radao.Models;
 
 
 namespace Radao.Data
 {
+
+
+    /// <summary>
+    /// Represents the database context for the Radao project.
+    /// </summary>
     public class RadaoContext : DbContext
     {
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AuctionContext"/> class.
+        /// </summary>
+        /// <param name="options">The options to configure the database context.</param>
+        public RadaoContext(DbContextOptions<RadaoContext> options) : base(options)
+        { }
+
+        /// <summary>
+        /// Gets or sets the collection of Addresses in the database.
+        /// </summary>
+        public DbSet<Address> Addresses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the collection of ContinuousUseDevices in the database.
+        /// </summary>
+        public DbSet<ContinuousUseDevice> ContinuousUseDevices { get; set; }
+
+        /// <summary>
+        /// Gets or sets the collection of Devices in the database.
+        /// </summary>
+        public DbSet<Device> Devices { get; set; }
+
+        /// <summary>
+        /// Gets or sets the collection of Fountains in the database.
+        /// </summary>
+        public DbSet<Fountain> Fountains { get; set; }
+
+        /// <summary>
+        /// Gets or sets the collection of WaterAnalysises in the database.
+        /// </summary>
+        public DbSet<WaterAnalysis> WaterAnalysises { get; set; }
 
     }
 }

337f278c640556c5e833dde4b57a1c2bfe3ba0da
Fábio Lindeza <129009704+fabionlindeza@users.noreply.github.com>
2025-02-13 14:53:06 +0000

    Added ContinuousUseDevice model with properties
    - Created the ContinuousUseDevice class with properties for Fountain, FountainId, AnalysisFrequency, and LastAnalysisDate.
    - Applied [Required] attribute to ensure Fountain, AnalysisFrequency, and LastAnalysisDate are not null.
    - Established a relationship with the Fountain model via FountainId.

--- Radao/Models/ContinuousUseDevice.cs
+++ Radao/Models/ContinuousUseDevice.cs
@@ -1,6 +1,18 @@
-﻿namespace Radao.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Radao.Models
 {
     public class ContinuousUseDevice
     {
+        [Required]
+        public Fountain Fountain { get; set; }
+
+        public int FountainId { get; set; }
+
+        [Required]
+        public int AnalysisFrequency { get; set; }
+
+        [Required]
+        public DateOnly LastAnalysisDate { get; set; }
     }
 }
